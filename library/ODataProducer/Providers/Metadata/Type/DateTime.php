1969a0d59ce46d1b39a87fb972a1f03f42e1697d
mrsafraz <mr.safraz@gmail.com>
2014-05-12 22:59:54 +0530

    Validate regex updated to support other formats
    support dates with 'Z' e.g: "2013-09-25T00:00:00.000Z" and other valid ISO formats

--- library/ODataProducer/Providers/Metadata/Type/DateTime.php
+++ library/ODataProducer/Providers/Metadata/Type/DateTime.php
@@ -84,8 +84,11 @@ class DateTime implements IType
         //1. The datetime value present in the $filter option should have 
         //   'datetime' prefix.
         //2. Month and day should be two digit
-        if (!preg_match("/^datetime\'(\d{4})-(\d{2})-(\d{2})((\s|T)([0-1][0-9]|2[0-4]):([0-5][0-9])(:([0-5][0-9])([Z])?)?)?\'$/", strval($value), $matches)
-        ) {
+ 
+        // support dates with 'Z' e.g: "2013-09-25T00:00:00.000Z" and other valid ISO formats
+        $pattern = '([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?';
+        if (!preg_match("/^datetime\'".$pattern."\'$/", strval($value), $matches))
+        {
             return false;
         } 
 
